e67979e5d1efbb776cb835f5e7ffee3e0484517f
YaroslavKovalchuk <35632982+YaroslavKovalchuk@users.noreply.github.com>
2019-01-08 22:23:48 +0200

    new prop

--- shop/src/main/java/goods/Properties/BudSize.java
+++ shop/src/main/java/goods/Properties/BudSize.java
@@ -1,12 +1,12 @@
 package goods.properties;
 
 public class BudSize implements FlowersProperty{
+
     private double size; //cm^3
 
     public BudSize(double size) {
         this.size = size;
     }
-
 
     public void printProperty() {
         System.out.println("size of the bud is " + size + " cubic centimeters");
